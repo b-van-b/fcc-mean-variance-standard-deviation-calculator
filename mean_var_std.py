124c9df2b5ad207039dcd890533ac75a3324a611
Replit user <>
2022-11-06 15:59:08 +0000

    feat: implement #calculate(arr)

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,6 +1,52 @@
 import numpy as np
 
 
-def calculate(list):
+def calculate(arr):
+    try:
+        matrix = np.array(arr, dtype=int).reshape(3,3)
+    except ValueError:
+        raise ValueError("List must contain nine numbers.")
+
+    calculations = {
+        'mean': [
+            list(matrix.mean(axis=0)),
+            list(matrix.mean(axis=1)),
+            matrix.mean()
+        ],
+        'variance': [
+            list(matrix.var(axis=0)),
+            list(matrix.var(axis=1)),
+            matrix.var()
+        ],
+        'standard deviation': [
+            list(matrix.std(axis=0)),
+            list(matrix.std(axis=1)),
+            matrix.std()
+        ],
+        'max': [
+            list(matrix.max(axis=0)),
+            list(matrix.max(axis=1)),
+            matrix.max()
+        ],
+        'min': [
+            list(matrix.min(axis=0)),
+            list(matrix.min(axis=1)),
+            matrix.min()
+        ],
+        'sum': [
+            list(matrix.sum(axis=0)),
+            list(matrix.sum(axis=1)),
+            matrix.sum()
+        ],
+    }
 
     return calculations
+
+# {
+#   'mean': [axis1, axis2, flattened],
+#   'variance': [axis1, axis2, flattened],
+#   'standard deviation': [axis1, axis2, flattened],
+#   'max': [axis1, axis2, flattened],
+#   'min': [axis1, axis2, flattened],
+#   'sum': [axis1, axis2, flattened]
+# }
